d3d56a0338b15d5d4b0bb62410027d0bf0c4fb76
Edward Knueppel <edward.knueppel@zywave.com>
2021-12-15 23:20:00 -0600

    docs and refactor

--- finite_state_machine.py
+++ finite_state_machine.py
@@ -1,10 +1,26 @@
 class FiniteStateMachine:
-    def __init__(self, rules, initial):
+    def __init__(self, rules: dict, initial: str):
+        '''
+        Creates a finite state machine with
+        and inital state and a set of rules
+        to control state based on an input
+
+        keyword arguments
+        inital: the intial state of the machine
+        rules: the rules for setting state
+        '''
         self.state = initial
         self.rules = rules
 
-    def next_method(self, i):
+    def next(self, i: str):
+        '''
+        Gets the next state based on the current state
+        and an input
+
+        keyword arguments
+        i: the input to determine the next state
+        '''
         current_state = self.state
         next_state = self.rules[current_state][i]
         self.state = next_state
-        print(i, ":",current_state,"->",next_state)
+        print(i, ":", current_state, "->", next_state)
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import json
 from finite_state_machine import FiniteStateMachine
 
+
+#load the machine and input data
 file = open('data.json')
 data = json.load(file)
 file.close()
 
-initial = data["initial"]
-rules = data["rules"]
-inputs = data["inputs"]
+#Create new machine
+fsm = FiniteStateMachine(data["rules"], data["initial"])
 
-fsm = FiniteStateMachine(rules, initial)
-for i in inputs:
-    fsm.next_method(i)
+#Run machine on inputs
+for i in data["inputs"]:
+    fsm.next(i)
